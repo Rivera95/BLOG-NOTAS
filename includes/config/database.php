b82567a12db00ebb64b29bc746805129db92517a
Cristian Javier Rivera Buritica <crivera@tiqal.com>
2024-08-23 15:08:57 -0500

    FEAT: Se agrega nueva funcionalidades como autenticacion, save e identificar el correo
    Signed-off-by: Cristian Javier Rivera Buritica <crivera@tiqal.com>

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function connectDB() : mysqli
 {
-    $db = mysqli_connect('localhost', 'root','','ejercicio1');
+    $db = mysqli_connect('localhost', 'root','','blog_notas');
 
     if(!$db){
     echo "Error";
--- src/models/User.php
+++ src/models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+
 class User 
 {
     private $db;
@@ -26,6 +27,56 @@ class User
     public function validate()
     {
         $errors = [];
+
+        if (empty($this->email)) {
+            $errors[] = 'El correo electronico es oblihatorio';
+        } elseif (!filter_var($this->email,FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'El correo electronico no esvalido';
+        }
+
+        if (empty($this->password)) {
+            $errors[] = 'La contraseña es obligatoria';
+        } elseif (strlen($this->password) < 6 ) {
+            $errors[] = 'La contrasena debe tener al menos 6 caracteres';
+        }
+
+        return $errors;
+    }
+
+    public function save()
+    {
+        if (!empty($this->validate())) {
+            return false;
+        }
+
+        $hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
+        $query = $this->db->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+
+        if (!$query) {
+            throw new \Exception("Error al preparar la consulta" . $this->db->error);
+        }
+
+        $query->bind_param("ss",$this->email, $hashedPassword);
+
+        if (!$query->execute()) {
+            throw new \Exception("Errro al ejecutar la consulta". $this->db->error);
+        }
+    }
+
+    public function authenticate()
+    {
+        $query = $this->db->prepare("SELECT FROM users WHERE email = ? LIMIT 1");
+        $query->bind_param("s",$this->email);
+        $query->execute();
+        $result = $query->get_result();
+
+        if ($result->num_rows === 1) {
+            $user = $result->fetch_assoc();
+            if (password_verify($this->password, $user["password"])) {
+                return $user;
+            }
+        }
+        return false;
     }
 
 }
